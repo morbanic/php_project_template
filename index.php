072690d3a829ff41ca3e8831c271a4efb2273c1e
Masimo Orbanic <masimo@Masimos-MBP.lan>
2018-09-13 07:22:49 +0200

    change

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php  
 
 if(session_status() == PHP_SESSION_NONE) session_start();
+
+
 require_once('config.php');
 
 use User\User as User;
 use Business\Business as Business;
 use Api\HNBTecaj as HNBTecaj;
 
+
+
 $user = new User("Test1");
 $business = new Business("student");
 $user->setBusiness($business);
+
+
+
 echo $user->getName()  . " is " . $user->getBusiness()->getName();
 
 
